63587dc7b24e757ed39dfa6a39c92ebdc483e750
pemrouz <p3pedram@hotmail.com>
2015-11-18 03:08:39 +0000

    init commit

---
+++ test.js
@@ -0,0 +1,30 @@
+var expect = require('chai').expect
+  , defaults = require('./')
+
+describe('defaults', function() {
+  
+  it('should default to preset if not set', function() {
+    var el = {}
+      , opts = { foo: 'bar' }
+
+    var state = defaults(el, opts)
+    expect(el).to.be.eql({ state: { foo: 'bar'} })
+  })
+  
+  it('should use datum if present', function() {
+    var el = { __data__: { foo: 'baz' } }
+      , opts = { foo: 'bar' }
+
+    var state = defaults(el, opts)
+    expect(el).to.be.eql({ __data__: { foo: 'baz' }, state: { foo: 'baz'} })
+  })
+
+  it('should use data over state, state over preset', function() {
+    var el = { __data__: { foo: 'baz' }, state: { foo: 'boo', a: 1 } }
+      , opts = { foo: 'bar', a: 2, b: 3 }
+
+    var state = defaults(el, opts)
+    expect(el).to.be.eql({ __data__: { foo: 'baz', a: 1, b: 3 }, state: { foo: 'baz', a: 1, b: 3 } })
+  })
+
+})
