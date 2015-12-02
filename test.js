c324bc79abe91953c13d261fc5c75008b1a2c64b
pemrouz <p3pedram@hotmail.com>
2015-12-02 15:34:23 +0000

    change implementation to allow single/multiple, and make props enumerable

--- test.js
+++ test.js
@@ -3,35 +3,29 @@ var expect = require('chai').expect
 
 describe('defaults', function() {
   
-  it('should default to preset if not set', function() {
-    var el = {}
+  it('should default singular', function() {
+    var o = {}
+    var foo = defaults(o, 'foo', 'bar')
+    expect(foo).to.be.eql('bar')
+    expect(o).to.be.eql({ foo: 'bar'})
+  })
+
+
+  it('should default plural', function() {
+    var o = {}
       , opts = { foo: 'bar' }
 
-    var state = defaults(el, opts)
-    expect(el).to.be.eql({ state: { foo: 'bar'} })
+    var state = defaults(o, { 
+      foo: 'bar' 
+    , bar: 'baz'
+    })
+
+    expect(o).to.not.be.equal(opts)
+    expect(o).to.be.eql(state)
+    expect(o).to.be.eql({ 
+      foo: 'bar' 
+    , bar: 'baz' 
+    })
   })
   
-  it('should use datum if present', function() {
-    var el = { __data__: { foo: 'baz' } }
-      , opts = { foo: 'bar' }
-
-    var state = defaults(el, opts)
-    expect(el).to.be.eql({ __data__: { foo: 'baz' }, state: { foo: 'baz'} })
-  })
-
-  it('should use data over state, state over preset', function() {
-    var el = { __data__: { foo: 'baz' }, state: { foo: 'boo', a: 1 } }
-      , opts = { foo: 'bar', a: 2, b: 3 }
-
-    var state = defaults(el, opts)
-    expect(el).to.be.eql({ __data__: { foo: 'baz', a: 1, b: 3 }, state: { foo: 'baz', a: 1, b: 3 } })
-  })
-
-  it('should return data/state if no opts', function() {
-    var el = { __data__: { foo: 'baz' }, state: { foo: 'boo', a: 1 } }
-      
-    var state = defaults(el)
-    expect(el).to.be.eql({ __data__: { foo: 'baz', a: 1 }, state: { foo: 'baz', a: 1 } })
-  })
-
 })
