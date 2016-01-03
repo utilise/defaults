d33c0d9cd07176a3a6cba465003368666999e493
pemrouz <p3pedram@hotmail.com>
2016-01-03 16:17:26 +0000

    reflect on host vs shadow and always return original value

--- test.js
+++ test.js
@@ -10,7 +10,6 @@ describe('defaults', function() {
     expect(o).to.be.eql({ foo: 'bar'})
   })
 
-
   it('should default plural', function() {
     var o = {}
       , opts = { foo: 'bar' }
@@ -28,4 +27,25 @@ describe('defaults', function() {
     })
   })
   
+  it('should not overwrite', function() {
+    var o = {}
+    var foo 
+
+    foo = defaults(o, 'foo', 'bar')
+    expect(foo).to.be.eql('bar')
+    expect(o).to.be.eql({ foo: 'bar'})
+
+    foo = defaults(o, 'foo', 'baz')
+    expect(foo).to.be.eql('bar')
+    expect(o).to.be.eql({ foo: 'bar'})
+  })
+  
+  it('should not bind on shadow root', function() {
+    var o = { host: {} }
+    defaults(o, { api: String })
+
+    expect(o.api).to.not.be.ok
+    expect(o.host.api).to.be.eql(String)
+  })
+
 })
